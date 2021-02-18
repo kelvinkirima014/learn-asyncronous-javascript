b7d975029cda3549901772a9365f11eabbcdb36a
kelvinkirima014 <kelvinkirima2@gmail.com>
2021-02-18 09:47:28 +0300

    more explanation

--- async.js
+++ async.js
@@ -4,21 +4,28 @@
 //now let's write the previous code asynchronously
 //let's use the setTimeOut function
 const secondMovie = () => {
-    // in this setTimeout we pass a callback function thar runs after 1s
-    setTimeout(() => {
-        console.log('second movie prints after 1s')
+    // in this setTimeout we pass a callback function that runs after 1s
+   setTimeout(() => {
+       console.log('second movie prints after 1s')
+
     },1000)
+
 }
 secondMovie()
+
 const firstMovie = () => {
     console.log ('display this movie first')
 
+
 }
 firstMovie()
-
-
-    
-
-
-
-
+// let's look at another setTimeout
+//we set it to 0ms but the event loop waits for the
+//stack to clear before the callback can be popped on it again
+const getTown = () => {
+    setTimeout(() =>{
+        console.log ('this is k-town')
+    },0)
+}
+getTown()
+console.log('printnt l-town first')
